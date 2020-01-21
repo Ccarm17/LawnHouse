523c99d3f78fb4024c436e1b617e6deee6f6a235
DESKTOP-VR5VUFR\Clay <cgriffithcarmichael@gmail.com>
2020-01-21 13:01:34 +1000

    changing the decimal place

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ from Adafruit_IO import Client, RequestError, Feed
 
 # the header names for the csv file
 HEADER = ['Temperature', 'Temperature1', 'Humidity', 'Humidity1', 'Time']
-sleepTime = 2
+sleepTime = 20
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
@@ -20,7 +20,7 @@ aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 loopcount = 0
 
 try:
-    humidity1 = aio.feeds('humidity')
+    humidity = aio.feeds('humidity')
 except RequestError: # Doesn't exist, create a new feed
     feed = Feed(name="humidity")
     humidity = aio.create_feed(feed)
@@ -49,10 +49,10 @@ while True:
     print("\nTemperature: %0.1f C" % sensor1.temperature)
     print("Humidity: %0.1f %%" % sensor1.relative_humidity)
 
-    aio.send_data(temperature.key, sensor.temperature)
-    aio.send_data(temperature1.key, sensor1.temperature)
-    aio.send_data(humidity.key, sensor.relative_humidity)
-    aio.send_data(humidity1.key, sensor1.relative_humidity)
+    aio.send_data(temperature.key, "%0.1f" % sensor.temperature)
+    aio.send_data(temperature1.key, "%0.1f" % sensor1.temperature)
+    aio.send_data(humidity.key,"%0.1f" % sensor.relative_humidity)
+    aio.send_data(humidity1.key,"%0.1f" % sensor1.relative_humidity)
 
     loopcount += 1
     time.sleep(sleepTime)
