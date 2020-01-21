80effc7dc40b30e74d0e81905713c6de37703e6f
DESKTOP-VR5VUFR\Clay <cgriffithcarmichael@gmail.com>
2020-01-21 11:58:20 +1000

    add io

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import board
 import busio
 import adafruit_sht31d
 import csv
+import Adafruit_IO
 
 # the header names for the csv file
 HEADER = ['Temperature', 'Temperature1', 'Humidity', 'Humidity1', 'Time']
@@ -12,15 +13,43 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
 sensor1 = adafruit_sht31d.SHT31D(i2c)
 
+ADAFRUIT_IO_KEY = "aio_hAvt22je34xRKA3YFj302pNn67OG"
+ADAFRUIT_IO_USERNAME = "Ccarm97"
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
 loopcount = 0
 
+try:
+    humidity1 = aio.feeds('humidity')
+except RequestError: # Doesn't exist, create a new feed
+    feed = Feed(name="humidity")
+    humidity = aio.create_feed(feed)
 
+try:
+    humidity1 = aio.feeds('humidity1')
+except RequestError: # Doesn't exist, create a new feed
+    feed = Feed(name="humidity1")
+    humidity1 = aio.create_feed(feed)
+
+try:
+    temperature = aio.feeds('temperature')
+except RequestError: # Doesn't exist, create a new feed
+    feed = Feed(name="temperature")
+    temperature = aio.create_feed(feed)
+
+try:
+    temperature1 = aio.feeds('temperature1')
+except RequestError: # Doesn't exist, create a new feed
+    feed = Feed(name="temperature1")
+    temperature1 = aio.create_feed(feed)
 
 while True:
     print("\nTemperature: %0.1f C" % sensor.temperature)
     print("Humidity: %0.1f %%" % sensor.relative_humidity)
     print("\nTemperature: %0.1f C" % sensor1.temperature)
     print("Humidity: %0.1f %%" % sensor1.relative_humidity)
+
+    aio.send_data(temperature.key, sensor.temperature)
 
     loopcount += 1
     time.sleep(sleepTime)
